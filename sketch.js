7c4f6b59c0294a71f39f4ca1e2e988a2eca0a985
deblinac27 <68397818+deblinac27@users.noreply.github.com>
2020-09-13 10:13:09 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ var divisionHeight = 300;
 var particles = [];
 var plinkos = [];
 var divisions = [];
-console.log(divisions);
+score = 0;
 function preload(){
 
 }
@@ -19,21 +19,8 @@ function setup() {
   world = engine.world;
 
   ground = new Ground(240, 785, 480, 10);
-  
-}
-
-function draw() {
-  background(0);  
-  Engine.update(engine);
-
-  ground.display();
-  
   for (var k = 0; k <= width; k = k + 80){
     divisions.push(new Divisions(k, 630, 10, divisionHeight))
-  }
-
-  for (var k = 0; k < divisions.length; k++){
-    divisions[k].display();
   }
 
   for (var j = 40; j<=width; j=j+50){
@@ -52,16 +39,29 @@ function draw() {
     plinkos.push(new Plinko(j,375, 12));
   }
 
+}
+
+function draw() {
+  background(0);  
+  Engine.update(engine);
+
+  ground.display();
+
   for (var j = 0; j < plinkos.length; j++){
     plinkos[j].display();
   }
 
-  if (frameCount%10===0){
-    particles.push(new Particle(random(width/2-10, width/2+10), 10, 12));
+  if (frameCount%60===0){
+    particles.push(new Particle(random(width/2-30, width/2+30), 10, 12));
+    score++;
   }
 
-  if (frameCount%10===0){
-    particles[particles].display();
+  for (var j = 0; j < particles.length; j++){
+    particles[j].display();
+  }
+
+  for (var k = 0; k < divisions.length; k++){
+    divisions[k].display();
   }
   drawSprites();
 }
